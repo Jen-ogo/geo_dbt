4aa506e0269a1f2211f8d27941381158a2c3236f
Eugene Ogorodnik <eugene.frd@pm.me>
2026-01-13 21:33:18 +0000

    changes in bronze-silver-gold

--- models/gold/gold_feature_h3_buildings_r10/feat_h3_buildings_R10.sql
+++ models/gold/gold_feature_h3_buildings_r10/feat_h3_buildings_R10.sql
@@ -1,5 +1,5 @@
 SELECT
-  c.region,
+  c.region_code,
   c.h3_r10,
   c.cell_area_m2,
   c.cell_wkt_4326,
@@ -14,11 +14,11 @@ SELECT
   a.levels_p50,
   a.footprint_area_p50_m2,
   a.footprint_area_p90_m2,
-  a.buildings_cnt / NULLIF(c.cell_area_m2 / 1e6, 0)         AS buildings_per_km2,
-  a.footprint_area_m2_sum / NULLIF(c.cell_area_m2 / 1e6, 0) AS footprint_m2_per_km2,
-  a.floor_area_m2_est_sum / NULLIF(c.cell_area_m2 / 1e6, 0) AS floor_area_m2_per_km2,
-  a.footprint_area_m2_sum / NULLIF(c.cell_area_m2, 0)       AS built_up_share,
+  a.buildings_cnt / NULLIF(c.cell_area_m2 / 1e6, 0)            AS buildings_per_km2,
+  a.footprint_area_m2_sum / NULLIF(c.cell_area_m2 / 1e6, 0)    AS footprint_m2_per_km2,
+  a.floor_area_m2_est_sum / NULLIF(c.cell_area_m2 / 1e6, 0)    AS floor_area_m2_per_km2,
+  a.footprint_area_m2_sum / NULLIF(c.cell_area_m2, 0)          AS built_up_share,
   a.last_load_ts
 FROM {{ ref('cell_area_ref_feat_h3_buildings_R10') }} c
 JOIN {{ ref('agg_levels_area_count_ref_foot_area') }} a
-  ON a.region = c.region AND a.h3_r10 = c.h3_r10
+  ON a.region_code = c.region_code AND a.h3_r10 = c.h3_r10
